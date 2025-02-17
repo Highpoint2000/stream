3ab69f034aa854301be45e97e8d0d125cbbc155f
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2025-02-17 16:49:32 +0100

    Update StreamPlugin.js

--- StreamPlugin.js
+++ StreamPlugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Stream',
-    version: '1.0',
+    version: '1.1',
     author: 'Highpoint',
     frontEndPath: 'Stream/stream.js'
 }
